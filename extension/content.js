5ab6aa913f9c4474176071ab5520ef86c15f05c7
Padmashree Jha <codewithpom@gmail.com>
2023-12-14 07:23:12 +0000

    s

--- extension/content.js
+++ extension/content.js
@@ -8,17 +8,18 @@ function handleHover(event) {
     const link = hoveredLink.href;
     
     // make fetch request to https://localgost:4567/expand?url=link
+    hoveredLink.removeEventListener('mouseover', handleHover);
     fetch(`http://localhost:4567/expand?url=${link}`)
         .then(response => response.json())
         .then(data => {
             // Get the expanded url
             const expandedUrl = data.r;
             // Set the title attribute of the hovered link to the expanded url
-            hoveredLink.title += `\nShortener Pro:\nLink: expandedUrl\n`;
+            hoveredLink.title += `\nShortener Pro:\nLink: ${expandedUrl}\n`;
         })
         .catch(error => console.log(error));
         // remove this event listener
-        hoveredLink.removeEventListener('mouseover', handleHover);
+    
 }
 
 
